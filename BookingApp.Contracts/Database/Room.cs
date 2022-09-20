1c35744dd9dafc82a7f50943e2e337c9e8215f50
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-09-20 15:04:20 +0300

    db schema

--- BookingApp.Contracts/Database/Room.cs
+++ BookingApp.Contracts/Database/Room.cs
@@ -1,7 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BookingApp.Contracts.Database;
 
+[Table("tbl_rooms", Schema = "public")]
 public class Room
 {
+    [Required]
+    [MaxLength(100)]
+    [Column("name")]
     public string Name { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [Column("type")]
     public string Type { get; set; }
+
+    [Required]
+    [Column("price")]
+    public int Price { get; set; }
+
 }
