f2948fa39296d07938fde74db072f61c301f7cb9
WIN-DNELCN2H2AJ\User <alvl.grabovsky@gmail.com>
2022-09-20 17:58:24 +0300

    migrations with models

--- BookingApp.Domain/Database/BookingDbContext.cs
+++ BookingApp.Domain/Database/BookingDbContext.cs
@@ -14,9 +14,4 @@ public class BookingDbContext : DbContext
     public BookingDbContext(DbContextOptions<BookingDbContext> options) : base(options)
     {
     }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql("User ID=postgres;Password=1234;Host=localhost;Port=5432;Database=bookingdb;Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0;");
-    }
 }
